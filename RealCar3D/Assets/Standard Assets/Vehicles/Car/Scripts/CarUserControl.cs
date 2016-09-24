12aaf5ab2fb6f616013b8e180a99d9d8cf3b7db4
mihail madzharov <mihail madzharov>
2016-09-24 20:20:21 +0300

    Another car

--- RealCar3D/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ RealCar3D/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -8,11 +8,14 @@ namespace UnityStandardAssets.Vehicles.Car
     [RequireComponent(typeof(CarController))]
     public class CarUserControl : MonoBehaviour
     {
-        public WebCom webCom;
+        public CarAIControl[] aiCars;
+
         private CarController m_Car; // the car controller we want to use
         private float forWard = 0;
         private float direction = 0;
         private string carData;
+        private float startTime = 10;
+        private bool started;
         private void Awake()
         {
             m_Car = GetComponent<CarController>();
@@ -22,6 +25,19 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void FixedUpdate()
         {
+
+
+            if (startTime <= 0 && !started)
+            {
+                for (int i = 0; i < aiCars.Length; i++)
+                {
+                    aiCars[i].enabled = true;
+                }
+                started = true;
+            }
+
+
+            startTime -= Time.deltaTime;
 
             float h = InputController.instance.GetVertical();
             float v = InputController.instance.GetHorizontal();
